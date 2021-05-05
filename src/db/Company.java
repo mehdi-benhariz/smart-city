da6a9beb21ee347e0bff8904bf8abb9220e6f8ba
mehdi-bh <benharizmehdi20@gmail.com>
2021-05-05 13:39:41 +0100

    finished job and company classes

--- src/db/Company.java
+++ src/db/Company.java
@@ -1,3 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Map;
+
 public class Company extends Place {
+    // first approach : this requires another
+    private List<Job> jobs = new ArrayList<Job>() {
+        {
+            add(new Job("developer", "description", "CS"));
+            add(new Job("QA", "description", "CS"));
+            add(new Job("Engineer", "description", "CS"));
+        }
+    };
+
+    // second approach
+    /* private Map<String, String> Jobs = new Map<String, String>(); */
+
+    public Company(Map.Entry<String, Integer> location, String adresse, String name, float ratings) {
+        /* super(location, adresse, name, ratings); */
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
 }
