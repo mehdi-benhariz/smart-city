13a0218f9f56fa34df2f4b5c175b3ef23bc13717
meddeb23 <dalimeddeb132@gmial.com>
2021-05-10 16:09:17 +0100

    add user auth services

---
+++ src/services/userservices/AuthActions.java
@@ -0,0 +1,50 @@
+package services.userservices;
+
+import java.util.Scanner;
+
+import models.User;
+
+public class AuthActions {
+    /*
+     * Get user input and return User object if Exist
+     */
+
+    public static User loginUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Username : ");
+        String username = input.nextLine();
+        System.out.print("password : ");
+        String password = input.nextLine();
+        input.close();
+        return User.auth(username, password);
+    }
+
+    /*
+     * Get user input and return User object if user created successfullly
+     */
+
+    public static User registeUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = input.next();
+        System.out.println("Enter your email:");
+        String email = input.next();
+        System.out.println("Enter your age:");
+        int age = input.nextInt();
+        System.out.println("Enter your password:");
+        String pwd = input.next();
+        System.out.println("confirm your password:");
+        String confirmpwd = input.next();
+
+        while (confirmpwd != pwd) {
+            System.out.println("[error] Password and confirme password don't match, please try again");
+            System.out.println("Enter your password:");
+            pwd = input.next();
+            System.out.println("confirm your password:");
+            confirmpwd = input.next();
+        }
+        input.close();
+
+        return User.register(age, email, name, pwd);
+    }
+}
